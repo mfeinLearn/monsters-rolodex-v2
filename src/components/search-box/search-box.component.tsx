5db2e14862af2c1883504c378bde4835ed8713f4
Malcome Fein <mfein90@gmail.com>
2026-01-22 11:04:01 -0500

    add changes

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,69 +1,24 @@
-import './search-box.styles.css';
+import { ChangeEventHandler } from 'react';
 
-// interface ISeachBoxProps extends IChangeHandlerProps {
-//   className: string;
-//   placeholder?: string; // string | null
-// }
-
-interface ISeachBoxProps {
-  className: string;
-  placeholder?: string; // string | null
-}
-
-
-interface ISeachBoxProps {
-  onChangeHandler: (a: string) => void
-}
+import './earch-box.style.css';
 
 type SearchBoxProps = {
   className: string;
   placeholder?: string;
-  onChangeHandler: (a: string) => void
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
 }
 
-type CanadianAddress = {
-  street: string;
-  province: string;
-}
-
-type USAddress = {
-  street: string;
-  state: string;
-}
-
-type ItalianAddress = {
-  street: string;
-  region: string;
-}
-
-type NorthAmericanAddress = CanadianAddress | USAddress | ItalianAddress;
-
-const Address: NorthAmericanAddress = {
-  street: 'abds',
-  region: 'ldsjf'
-}
-
-
-const name: string = '1233455667'
-
-const func: (a: string, b: string, c: string) => void = (a,b,c) => {}
-
-const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => (
+const SearchBox = ({ 
+  className, 
+  placeholder, 
+  onChangeHandler 
+}: SearchBoxProps) => (
   <input
     className={`search-box ${className}`}
     type='search'
     placeholder={placeholder}
-    onChange={(e) => onChangeHandler(e)}
+    onChange={onChangeHandler}
   />
 );
 
 export default SearchBox;
-
-// const SearchBox = ({ className, placeholder, onChangeHandler }: ISeachBoxProps) => (
-//   <input
-//     className={`search-box ${className}`}
-//     type='search'
-//     placeholder={placeholder}
-//     onChange={(e) => onChangeHandler(e)}
-//   />
-// );
